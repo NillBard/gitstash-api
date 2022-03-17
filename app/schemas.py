d8aed5a1ca0555bc911272b850606ddda3fa6153
Kirill Kumma <kirill.kumma@gmail.com>
2022-03-18 01:50:12 +0300

    schemas: add UserSignUp and UserLogin

---
+++ app/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+
+class UserSignUp(UserLogin):
+    name: str
+
+
+class JWT(BaseModel):
+    accessToken: str
+    refreshToken: str
