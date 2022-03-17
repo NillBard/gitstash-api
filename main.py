ebdd6e1ac215ee0ce55de4dee1d7dd5d277e4d3a
Kirill Kumma <kirill.kumma@gmail.com>
2022-03-18 01:49:12 +0300

    app: fix uvicorn usage in main

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ load_dotenv()
 PORT = int(os.getenv('PORT', default='8000'))
 
 if __name__ == '__main__':
-    uvicorn('app.api:app', host='0.0.0.0', port=PORT)
+    uvicorn.run('app.api:app', host='0.0.0.0', port=PORT)
